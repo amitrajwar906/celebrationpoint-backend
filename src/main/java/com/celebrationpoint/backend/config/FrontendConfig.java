1ede56f05d215020ea7248ca9b153dc378a79268
Amit Rajwar <amitxrajwar@gmail.com>
2026-01-26 11:51:44 -0800

    first commit

--- src/main/java/com/celebrationpoint/backend/config/FrontendConfig.java
+++ src/main/java/com/celebrationpoint/backend/config/FrontendConfig.java
@@ -30,7 +30,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class FrontendConfig {
 
-    @Value("${frontend.url:http://localhost:5173}")
+    @Value("${frontend.url:https://celebrationpointdevtestforclient.netlify.app}")
     private String frontendUrl;
 
     /**
